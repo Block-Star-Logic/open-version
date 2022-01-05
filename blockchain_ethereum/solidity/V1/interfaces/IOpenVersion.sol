3211dff2ed53298e2777f26d58f54dd96551cfea
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2022-01-05 22:38:19 +0000

    Updated

---
+++ blockchain_ethereum/solidity/V1/interfaces/IOpenVersion.sol
@@ -0,0 +1,12 @@
+//"SPDX-License-Identifier: APACHE 2.0"
+
+pragma solidity >=0.8.0 <0.9.0;
+
+
+interface IOpenVersion { 
+
+    function getVersion() view external returns (uint256 _version);
+
+    function getName() view external returns (string memory _contractName);
+
+}
